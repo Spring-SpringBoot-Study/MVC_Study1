6624838ae4f0fde5bb5594103dff2c6c2bbe364e
ChaeHyunLim <chaehyun010104@gmail.com>
2026-04-17 13:40:57 +0900

    프로젝트 초기 생성

---
+++ src/main/java/com/example/spring_mvc_study1_mvc/SpringMvcStudy1MvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_study1_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStudy1MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcStudy1MvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_mvc_study1_mvc/SpringMvcStudy1MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_study1_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcStudy1MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
